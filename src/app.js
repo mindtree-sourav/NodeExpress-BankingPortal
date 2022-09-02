7e5f833b93049e54d5b423715969d04f6a72eebc
Sourav Maity <sourav.maity2@mindtree.com>
2022-09-02 13:46:17 +0530

    Stable Release : Module5

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const { accounts, users, writeJSON } = require('./data.js');
 const express = require('express');
 const app = express();
 
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
+
 //View
 const viewPath = path.join(__dirname, 'views');
 app.set('view engine', 'ejs');
@@ -15,33 +18,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Middleware
 app.use(express.urlencoded({extended:true}));
 
-
 //Index Route
 app.get("/", function(req, res) {
     res.render('index', {
         title: 'Account Summary',
         accounts: accounts
-    })
-})
-
-//Savings Route
-app.get("/savings", function(req, res) {
-    res.render('account', {
-        account: accounts.savings
-    })
-})
-
-//Checking Route
-app.get("/checking", function(req, res) {
-    res.render('account', {
-        account: accounts.checking
-    })
-})
-
-//Credit Route
-app.get("/credit", function(req, res) {
-    res.render('account', {
-        account: accounts.credit
     })
 })
 
@@ -52,29 +33,8 @@ app.get('/profile', function(req, res) {
     })
 })
 
-//Transfer Route
-app.get('/transfer', function(req, res) {
-    res.render('transfer');
-})
-
-app.post('/transfer', function(req, res) {
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('transfer', {message: 'Transfer Completed'});
-});
-
-//Payment Route
-app.get('/payment', (req, res)=> {
-    res.render('payment', { account: accounts.credit })
-})
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
-})
+app.use('/account', accountRoutes)
+app.use('/services', servicesRoutes)
 
 //Port
 app.listen(3000, function() {
